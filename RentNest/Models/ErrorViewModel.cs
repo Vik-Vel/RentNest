69c257f708bf8a12762b3b5778a87b0113cd0b1f
Viktoriya Veleva <vikiveleva54@gmail.com>
2025-01-21 06:27:04 +0200

    Create project.

---
+++ RentNest/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RentNest.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
