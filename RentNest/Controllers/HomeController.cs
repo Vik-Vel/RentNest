9c76b7d16ae8236026579c95ae360d4eb5a56e42
Viktoriya Veleva <vikiveleva54@gmail.com>
2025-01-24 05:14:48 +0200

    Home model, view, controller created.

--- RentNest/Controllers/HomeController.cs
+++ RentNest/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using RentNest.Core.Models.Home;
 using RentNest.Models;
 using System.Diagnostics;
 
@@ -15,7 +16,9 @@ namespace RentNest.Controllers
 
         public IActionResult Index()
         {
-            return View();
+            var model = new IndexViewModel();
+
+            return View(model);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
