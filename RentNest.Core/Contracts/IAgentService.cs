4b3af8b829d3cf0aa226c65d1f972896a4fbe639
Viktoriya Veleva <vikiveleva54@gmail.com>
2025-02-10 19:46:33 +0200

    Agent service added

---
+++ RentNest.Core/Contracts/IAgentService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentNest.Core.Contracts
+{
+    public interface IAgentService
+    {
+    }
+}
---
+++ RentNest.Core/Services/AgentService.cs
@@ -0,0 +1,13 @@
+﻿using RentNest.Core.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentNest.Core.Services
+{
+    public class AgentService : IAgentService
+    {
+    }
+}
