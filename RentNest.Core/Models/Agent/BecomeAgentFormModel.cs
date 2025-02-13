ae20d901fed5be4643a989a4e5a135023d641c8f
Viktoriya Veleva <vikiveleva54@gmail.com>
2025-02-13 22:05:05 +0200

    Become agent form model modified - add phone number property with constraints

--- RentNest.Core/Models/Agent/BecomeAgentFormModel.cs
+++ RentNest.Core/Models/Agent/BecomeAgentFormModel.cs
@@ -1,6 +1,18 @@
-﻿namespace RentNest.Core.Models.Agent
+﻿using System.ComponentModel.DataAnnotations;
+using static RentNest.Core.Constants.MessageConstants;
+using static RentNest.Infrastructure.Constants.DataConstants;
+
+namespace RentNest.Core.Models.Agent
 {
+    //Go into interface
     public class BecomeAgentFormModel
     {
+        [Required(ErrorMessage = RequiredMessage)]
+        [StringLength(AgentPhoneNumberMaxLength, 
+            MinimumLength = AgentPhoneNumberMinLength, 
+            ErrorMessage = LengthMessage)]
+        [Display(Name = "Phone number")]
+        [Phone]
+        public required string PhoneNumber { get; set; }
     }
 }
