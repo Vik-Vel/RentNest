784a1dad272677d4398104b952b22e2d3ca1afc5
Viktoriya Veleva <vikiveleva54@gmail.com>
2025-02-16 14:37:37 +0200

    AgentController - Become updated. Message constants updated

--- RentNest/Controllers/AgentController.cs
+++ RentNest/Controllers/AgentController.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using RentNest.Attributes;
 using RentNest.Core.Contracts;
 using RentNest.Core.Models.Agent;
 using System.Security.Claims;
+using static RentNest.Core.Constants.MessageConstants;
 
 namespace RentNest.Controllers
 {
@@ -13,22 +15,37 @@ namespace RentNest.Controllers
         {
             agentService = _agentService;
         }
+
         [HttpGet]
-        public async Task<IActionResult> Become()
+        [NotAnAgent]
+        public IActionResult Become()
         {
-            if (await agentService.ExistByIdAsync(User.Id()))
-            {
-                return BadRequest();
-            }
             var model = new BecomeAgentFormModel();
             return View(model);
         }
 
         [HttpPost]
+        [NotAnAgent]
         public async Task<IActionResult> Become(BecomeAgentFormModel model)
         {
+            if (await agentService.UserWithPhoneNumberExistsAsync(model.PhoneNumber))
+            {
+                ModelState.AddModelError(nameof(model.PhoneNumber), PhoneExists);
+            }
 
-            return RedirectToAction(nameof(HouseController.All), "House");
+            if (await agentService.UserHasRentsAsync(User.Id()))
+            {
+                ModelState.AddModelError("Error", HasRents);
+            }
+
+            if (ModelState.IsValid == false)
+            {
+                return View(model);
+            }
+
+            await agentService.CreateAsync(User.Id(), model.PhoneNumber);
+
+              return RedirectToAction(nameof(HouseController.All), "House");
         }
     }
 }
