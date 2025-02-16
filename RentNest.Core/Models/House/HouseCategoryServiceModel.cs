74fc24b0901b7147d2e3b444f7d11f0bb66fbc4a
Viktoriya Veleva <vikiveleva54@gmail.com>
2025-02-16 17:20:43 +0200

    HouseCategoryServiceModel created

---
+++ RentNest.Core/Models/House/HouseCategoryServiceModel.cs
@@ -0,0 +1,10 @@
+﻿namespace RentNest.Core.Models.House
+{
+    //This is for the dropdown
+    public class HouseCategoryServiceModel
+    {
+        public int Id { get; set; } 
+
+        public string Name { get; set; } = string.Empty;
+    }
+}
