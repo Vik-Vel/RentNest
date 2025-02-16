aa1db86c1d514a79101fc5e76c8af364e5e8358c
Viktoriya Veleva <vikiveleva54@gmail.com>
2025-02-16 14:26:30 +0200

    Not an agent attribute created

---
+++ RentNest/Attributes/NotAnAgentAttribute.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using RentNest.Core.Contracts;
+using System.Security.Claims;
+
+namespace RentNest.Attributes
+{
+    //NotAnAgentAttribute inherits ActionFilterAttribute, which means it can be used as an attribute ([NotAnAgent]) on a controller or method.
+    public class NotAnAgentAttribute : ActionFilterAttribute
+    {
+        //OnActionExecuting is executed before the action (Action) in the controller.
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            //Calls base.OnActionExecuting(context) to implement the default behavior of ActionFilterAttribute.
+            base.OnActionExecuting(context);
+
+            IAgentService? agentService = context.HttpContext.RequestServices.GetService<IAgentService>();
+
+            //Checks if IAgentService exists
+            if (agentService == null)
+            {
+                context.Result = new StatusCodeResult(StatusCodes.Status500InternalServerError);
+            }
+
+            // Checks if the user is an agent
+            if (agentService != null 
+                && agentService.ExistsByIdAsync(context.HttpContext.User.Id()).Result)
+            {
+                context.Result = new StatusCodeResult(StatusCodes.Status400BadRequest);
+            }
+
+        }
+    }
+}
