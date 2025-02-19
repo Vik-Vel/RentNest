347ce29992ca26369217c8b148d10af151e40a58
Viktoriya Veleva <vikiveleva54@gmail.com>
2025-02-19 20:42:56 +0200

    AllCategoriesAsync, CategoryExistsAsync, CreateAsync created

--- RentNest.Core/Contracts/IHouseService.cs
+++ RentNest.Core/Contracts/IHouseService.cs
@@ -1,9 +1,14 @@
 ﻿using RentNest.Core.Models.Home;
+using RentNest.Core.Models.House;
 
 namespace RentNest.Core.Contracts
 {
     public interface IHouseService
     {
         Task<IEnumerable<HouseIndexServiceModel>> LastThreeHousesAsync();
+        Task<IEnumerable<HouseCategoryServiceModel>> AllCategoriesAsync();
+        Task<bool> CategoryExistsAsync(int categoryId);
+        Task<int> CreateAsync(HouseFormModel model, int agentId);
+
     }
 }
