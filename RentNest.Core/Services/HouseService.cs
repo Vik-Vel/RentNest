4f32e33821b3e0af2a1f4babd8172bdfb307dcbc
Viktoriya Veleva <vikiveleva54@gmail.com>
2025-02-19 20:44:22 +0200

    Implemented in house service CreateAsync, CategoryExistsAsync, AllCategoriesAsync.

--- RentNest.Core/Services/HouseService.cs
+++ RentNest.Core/Services/HouseService.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using RentNest.Core.Contracts;
 using RentNest.Core.Models.Home;
+using RentNest.Core.Models.House;
 using RentNest.Infrastructure.Data.Common;
 using RentNest.Infrastructure.Data.Models;
 
@@ -14,6 +15,43 @@ namespace RentNest.Core.Services
         {
             repository = _repository;
         }
+
+        public async Task<IEnumerable<HouseCategoryServiceModel>> AllCategoriesAsync()
+        {
+            return await repository.AllReadOnly<Category>()
+                .Select(c => new HouseCategoryServiceModel()
+                {
+                    Id = c.Id,
+                    Name = c.Name,
+                })
+                .ToListAsync();
+        }
+
+        public async Task<bool> CategoryExistsAsync(int categoryId)
+        {
+            return await repository.AllReadOnly<Category>()
+                .AnyAsync(c => c.Id == categoryId);
+
+        }
+
+        public async Task<int> CreateAsync(HouseFormModel model, int agentId)
+        {
+            House house = new House()
+            {
+                Address = model.Address,
+                AgentId = agentId,
+                CategoryId = model.CategoryId,
+                Description = model.Description,
+                ImageUrl = model.ImageUrl,
+                PricePerMonth = model.PricePerMonth,
+                Title = model.Title
+            };
+            
+            await repository.AddAsync(house);
+            await repository.SaveChangesAsync();
+            return house.Id;
+        }
+
         public async Task<IEnumerable<HouseIndexServiceModel>> LastThreeHousesAsync()
         {
             return await repository
